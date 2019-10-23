58416ae3c3614ad31ee309e554099f3b3ca507e6
Марина Скрипкина <smk08300@mpei.ru>
2019-10-23 09:42:10 +0400

    first commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+ #include <iostream> 
+ 
+ using namespace std; 
+cout << "A + B = " << a + b << '\n';
+a*b; 
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+ 
